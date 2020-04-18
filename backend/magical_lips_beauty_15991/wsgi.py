be19eb7e8fc1f2317f1fdcc37b2332bbc0c7fba4
Crowdbotics <team@crowdbotics.com>
2020-04-18 23:42:49 +0000

    Update models

--- backend/magical_lips_beauty_15991/wsgi.py
+++ backend/magical_lips_beauty_15991/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magical_lips_beauty_15991.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "magical_lips_beauty_15991.settings")
 
 application = get_wsgi_application()
